6703f291a1a0f2970bd89aa4b5c2ccb92fb3efbd
mahimtalukder <mahim4163@gmail.com>
2023-06-04 09:38:24 +0000

    new file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-open_ai_key = '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'
+open_ai_key = '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'
 dataset = 'dataset/output.txt'
 PAGE_ACCESS_TOKEN = "EAANEIEXoO34BAF942F5Kbvm7Kpg66H0GzIt1aRSPye0AayBZCqGJ99jAYsTTZAVm7NkqUzEw9HJt194FVe1kZC9LIToLfjoZA2jQeU9ZC0396EKyjZC77WtsFNLHwvHW8Ycdan3qfjmLh3STSv1j4ZB4cZAltaRrm2VTjsuVr1yksuVsFYS1ZB1eUUALrsNi08rFUVSKk9yHwQgZDZD" 
 API = f"https://graph.facebook.com/v13.0/me/messages?access_token={PAGE_ACCESS_TOKEN}"
